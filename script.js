8100e1f2644730358027fc9965df5679fa368739
Oluwaferanmi Aiyegbusi <oluwaferanmiaiyegbusi@gmail.com>
2022-08-30 18:49:36 +0100

    classes section done

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 "use strict";
 const firstClass = document.querySelector(".tab1");
 const secondClass = document.querySelector(".tab2");
-const firstArticle = document.querySelector(".first-article")
-const secondArticle = document.querySelector(".second-article")
+const thirdClass = document.querySelector(".tab3");
+const fourthClass = document.querySelector(".tab4");
+const firstArticle = document.querySelector(".first-article");
+const secondArticle = document.querySelector(".second-article");
+const thirdArticle = document.querySelector(".third-article");
+const fourthArticle = document.querySelector(".fourth-article");
+
+firstClass.addEventListener("click", () => {
+  firstArticle.classList.remove("hidden");
+  secondArticle.classList.add("hidden");
+  thirdArticle.classList.add("hidden");
+  fourthArticle.classList.add("hidden");
+});
 
 secondClass.addEventListener("click", () => {
   firstArticle.classList.add("hidden");
   secondArticle.classList.remove("hidden");
+  thirdArticle.classList.add("hidden");
+  fourthArticle.classList.add("hidden");
+});
+
+thirdClass.addEventListener("click", () => {
+  firstArticle.classList.add("hidden");
+  secondArticle.classList.add("hidden");
+  thirdArticle.classList.remove("hidden");
+  fourthArticle.classList.add("hidden");
+});
+
+fourthClass.addEventListener("click", () => {
+  firstArticle.classList.add("hidden");
+  secondArticle.classList.add("hidden");
+  thirdArticle.classList.add("hidden");
+  fourthArticle.classList.remove("hidden");
 });
