fcfbda4db5145cc57057669227f64621c3a1ad9b
Oluwaferanmi Aiyegbusi <oluwaferanmiaiyegbusi@gmail.com>
2022-09-08 21:57:49 +0100

    done withthe table part

--- script.js
+++ script.js
@@ -7,12 +7,35 @@ const firstArticle = document.querySelector(".first-article");
 const secondArticle = document.querySelector(".second-article");
 const thirdArticle = document.querySelector(".third-article");
 const fourthArticle = document.querySelector(".fourth-article");
+const innerColor1 = document.querySelector(".tab1");
+const innerColor2 = document.querySelector(".tab2");
+const innerColor3 = document.querySelector(".tab3");
+const innerColor4 = document.querySelector(".tab4");
+const monTable = document.querySelector(".mon");
+const monday = document.querySelector(".Monday");
+const tueTable = document.querySelector(".tue");
+const tuesday = document.querySelector(".Tuesday");
+const wedTable = document.querySelector(".wed");
+const wednesday = document.querySelector(".Wednesday");
+const thurTable = document.querySelector(".thur");
+const thursday = document.querySelector(".Thursday");
+const friTable = document.querySelector(".fri");
+const friday = document.querySelector(".Friday");
+const textColor1 = document.querySelector(".mon");
+const textColor2 = document.querySelector(".tue");
+const textColor3 = document.querySelector(".wed");
+const textColor4 = document.querySelector(".thur");
+const textColor5 = document.querySelector(".fri");
 
 firstClass.addEventListener("click", () => {
   firstArticle.classList.remove("hidden");
   secondArticle.classList.add("hidden");
   thirdArticle.classList.add("hidden");
   fourthArticle.classList.add("hidden");
+  innerColor1.style.color = "#fa3d7c";
+  innerColor2.style.color = "#000000";
+  innerColor3.style.color = "#000000";
+  innerColor4.style.color = "#000000";
 });
 
 secondClass.addEventListener("click", () => {
@@ -20,6 +43,10 @@ secondClass.addEventListener("click", () => {
   secondArticle.classList.remove("hidden");
   thirdArticle.classList.add("hidden");
   fourthArticle.classList.add("hidden");
+  innerColor2.style.color = "#fa3d7c";
+  innerColor1.style.color = "#000000";
+  innerColor3.style.color = "#000000";
+  innerColor4.style.color = "#000000";
 });
 
 thirdClass.addEventListener("click", () => {
@@ -27,6 +54,10 @@ thirdClass.addEventListener("click", () => {
   secondArticle.classList.add("hidden");
   thirdArticle.classList.remove("hidden");
   fourthArticle.classList.add("hidden");
+  innerColor3.style.color = "#fa3d7c";
+  innerColor2.style.color = "#000000";
+  innerColor1.style.color = "#000000";
+  innerColor4.style.color = "#000000";
 });
 
 fourthClass.addEventListener("click", () => {
@@ -34,4 +65,73 @@ fourthClass.addEventListener("click", () => {
   secondArticle.classList.add("hidden");
   thirdArticle.classList.add("hidden");
   fourthArticle.classList.remove("hidden");
+  innerColor4.style.color = "#fa3d7c";
+  innerColor3.style.color = "#000000";
+  innerColor2.style.color = "#000000";
+  innerColor1.style.color = "#000000";
+});
+
+monTable.addEventListener("click", () => {
+  monday.classList.remove("hidden");
+  tuesday.classList.add("hidden");
+  wednesday.classList.add("hidden");
+  thursday.classList.add("hidden");
+  friday.classList.add("hidden");
+  textColor1.style.color = "#fa3d7c";
+  textColor2.style.color = "#ffffff";
+  textColor3.style.color = "#ffffff";
+  textColor4.style.color = "#ffffff";
+  textColor5.style.color = "#ffffff";
+});
+
+tueTable.addEventListener("click", () => {
+  tuesday.classList.remove("hidden");
+  monday.classList.add("hidden");
+  wednesday.classList.add("hidden");
+  thursday.classList.add("hidden");
+  friday.classList.add("hidden");
+  textColor2.style.color = "#fa3d7c";
+  textColor1.style.color = "#ffffff";
+  textColor3.style.color = "#ffffff";
+  textColor4.style.color = "#ffffff";
+  textColor5.style.color = "#ffffff";
+});
+
+wedTable.addEventListener("click", () => {
+  wednesday.classList.remove("hidden");
+  tuesday.classList.add("hidden");
+  monday.classList.add("hidden");
+  thursday.classList.add("hidden");
+  friday.classList.add("hidden");
+  textColor3.style.color = "#fa3d7c";
+  textColor2.style.color = "#ffffff";
+  textColor1.style.color = "#ffffff";
+  textColor4.style.color = "#ffffff";
+  textColor5.style.color = "#ffffff";
+});
+
+thurTable.addEventListener("click", () => {
+  thursday.classList.remove("hidden");
+  tuesday.classList.add("hidden");
+  wednesday.classList.add("hidden");
+  monday.classList.add("hidden");
+  friday.classList.add("hidden");
+  textColor4.style.color = "#fa3d7c";
+  textColor2.style.color = "#ffffff";
+  textColor3.style.color = "#ffffff";
+  textColor1.style.color = "#ffffff";
+  textColor5.style.color = "#ffffff";
+});
+
+friTable.addEventListener("click", () => {
+  friday.classList.remove("hidden");
+  tuesday.classList.add("hidden");
+  wednesday.classList.add("hidden");
+  thursday.classList.add("hidden");
+  monday.classList.add("hidden");
+  textColor5.style.color = "#fa3d7c";
+  textColor2.style.color = "#ffffff";
+  textColor3.style.color = "#ffffff";
+  textColor4.style.color = "#ffffff";
+  textColor1.style.color = "#ffffff";
 });
