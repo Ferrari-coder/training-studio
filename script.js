880dc66769777040da6992a9432c1838a68047a6
Amudipe Emmanuel <abiemma84@gmail.com>
2022-09-19 14:28:59 +0100

    Done the nav bar

--- script.js
+++ script.js
@@ -30,20 +30,22 @@ const darkBody = document.querySelector("body");
 const moonie = document.querySelector(".fa-moon");
 const darkie = document.querySelectorAll(".texty");
 const burger = document.querySelector(".hamburger-body");
+const dropDown = document.querySelector(".drop_down-menu");
 
-
-burger.addEventListener("click", function() {
-  burger.classList.toggle("open")
-})
+burger.addEventListener("click", function () {
+  burger.classList.toggle("open");
+  dropDown.classList.toggle("show_dropdown");
+});
 
 console.log(darkie);
 
-  moonie.addEventListener("click", () => {
-    darkBody.classList.toggle("darkmode");
-    for (let i = 0; i < darkie.length; i++) {
-      darkie[i].classList.toggle("darkmode-colors")
-    }
-  });
+moonie.addEventListener("click", () => {
+  darkBody.classList.toggle("darkmode");
+  moonie.classList.toggle("dark_moon");
+  for (let i = 0; i < darkie.length; i++) {
+    darkie[i].classList.toggle("darkmode-colors");
+  }
+});
 
 firstClass.addEventListener("click", () => {
   firstArticle.classList.remove("hidden");
